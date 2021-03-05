e2f9fc332b7a82135eb1335a6aef882f40105128
韩晓峰 <2220342736@qq.com>
2021-03-05 16:39:03 +0800

    ssm框架整合逻辑代码提交

--- ssm/src/main/java/pers/xiaofeng/controller/AccountController.java
+++ ssm/src/main/java/pers/xiaofeng/controller/AccountController.java
@@ -1,7 +1,14 @@
 package pers.xiaofeng.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import pers.xiaofeng.domain.Account;
+import pers.xiaofeng.service.AccountService;
+
+import java.util.List;
 
 /**
  * @className: pers.xiaofeng.controller.AccountController
@@ -9,16 +16,28 @@ import pers.xiaofeng.domain.Account;
  * @author: xiaofeng
  * @create: 2021-03-04 17:37
  */
+@Controller
+@RequestMapping("/account")
 public class AccountController {
 
+    @Autowired
+    private AccountService accountService;
+
     // 保存
+    @RequestMapping("/save")
+    @ResponseBody
     public String save(Account account) {
-        return null;
+        accountService.save(account);
+        return "保存成功";
     }
 
     // 查询
     public ModelAndView findAll() {
-        return null;
+        List<Account> accountList = accountService.findAll();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("accountList", accountList);
+        modelAndView.setViewName("accountList");
+        return modelAndView;
     }
 
 }
--- ssm/src/main/java/pers/xiaofeng/service/impl/AccountServiceImpl.java
+++ ssm/src/main/java/pers/xiaofeng/service/impl/AccountServiceImpl.java
@@ -1,8 +1,16 @@
 package pers.xiaofeng.service.impl;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.stereotype.Service;
 import pers.xiaofeng.domain.Account;
+import pers.xiaofeng.mapper.AccountMapper;
 import pers.xiaofeng.service.AccountService;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -11,15 +19,37 @@ import java.util.List;
  * @author: xiaofeng
  * @create: 2021-03-04 17:36
  */
+@Service("accountService")
 public class AccountServiceImpl implements AccountService {
 
     @Override
     public void save(Account account) {
-
+        try {
+            InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapperConfig.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+            AccountMapper mapper = sqlSession.getMapper(AccountMapper.class);
+            mapper.save(account);
+            sqlSession.commit();
+            sqlSession.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<Account> findAll() {
+        try {
+            InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapperConfig.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+            AccountMapper mapper = sqlSession.getMapper(AccountMapper.class);
+            List<Account> accountList = mapper.findAll();
+            sqlSession.close();
+            return accountList;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
